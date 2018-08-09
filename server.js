4833368d736ab85b55e1d49f7762c814354f3de0
Umut Balkan <umutbalkan@icloud.com>
2018-08-09 14:19:18 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname +  '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = now + ":" + req.method + " "+ req.url;
+  console.log(log);
+  fs.appendFile('server.log', log + "\n", (err) => {
+    if(err){
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
+})
+
+app.get('/', (req,res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeText: 'Welcome to Homepage'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
